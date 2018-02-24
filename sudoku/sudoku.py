a74b47060e0b152c0e693d8bc58d6d09d6666548
Tony Tran <tonytrantrantony@gmail.com>
2018-02-24 22:48:20 +0100

    sudoku/sudoku.py: Completed board verification
    Modified dPrint function to print user-friendly when a specific
    argument is presented.
    
    Program supports two type of output, 'user-friendly' or 'simple'.
    Simple type of output are: "correct", "solved", "incorrect",
    "error".
    
    Load function ignores empty lines or lines filled with spaces.
    
    Added function "checkUnique" (line 66) which determine if a list
    contain only unique value (return None) or it has item duplicates
    (return True).
    
    Added function "getColumns" (line 72) which returns a 2D list of
    the current board game COLUMNS (default board is 2D list of ROWS).
    
    Added function "getBox" (line 81) which returns a 2D list of the
    current board game SMAAL (3x3) BOXES.
    
    Function "removeZero" (line 91) is modified to remove zeros from
    a list presented as a function argument. (before it takes the
    default board game list 'self.board')
    
    Added a 'except ValueError' with a custom error message (visible
    only when 'user-friendly' printing is on).

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -2,10 +2,24 @@
 
 import sys
 
-def dPrint(s):
+"""
+def dPrint(string,alternative=None):
     if __debug__:
         #stdout.write(s)
-        print s
+        print string
+    elif alternative:
+        print alternative
+"""
+
+def dPrint(string,alternative=None):
+    try:
+        if sys.argv[1] in ["-d","--details","details"]:
+            print string
+            alternative = None
+    except IndexError:
+        pass
+    if alternative:
+        print alternative
 
 class Game:
     def __init__(self):
@@ -13,56 +27,90 @@ class Game:
 
     def load(self):
         for line in sys.stdin:
+            if line.strip() == "":
+                continue
             self.board.append([int(n) for n in line.split()])
 
     def verify(self):
         #check number of row
         if len(self.board) != 9:
-            return
+            return "error","incorrect number of row"
 
         #check number of column
         for row in self.board:
             if len(row) != 9:
-                return
+                return "error","incorrect number of column"
 
         #check if number are from 0 - 9
         for row in self.board:
             for number in row:
-                if number < 0 or number > 9: #if number in xrange(0,10):
-                    return
+                if number not in range(10): #number < 0 or number > 9:
+                    return "error","an incorrect number detected"
 
         #check unique number
-        noZero = self.removeZero()
+        if self.checkUnique(self.board): #check in row
+            return "incorrect","repeated number in a row"
 
-        #---check in row
-        for row in noZero:
-            if len(row) != len(set(row)):
-                return
+        if self.checkUnique(self.getColumns()): #check in column
+            return "incorrect","repeated number in a column"
 
-        #---check in column
-        #rowN = 0
-        #while rowN < 9:
-        #    print noZero[rowN]
-        #    rowN = rowN + 1
+        if self.checkUnique(self.getBox()): #check in box
+            return "incorrect","repeated number in a small box"
 
-        return True
+        #check if any 'empty spots' (zeros)
+        for i in xrange(9):
+            if len(self.removeZero(self.board)[i]) != len(self.board[i]):
+                return "correct","" #there is zero therefore its not solved
+        return "solved","the game is already solved"
 
-    def removeZero(self):
-        list = []
-        for row in self.board:
-            list.append(filter(lambda n: n != 0, row))
-        return list
+    def checkUnique(self,list): #True = not unique, none = unique
+        list = self.removeZero(list)
+        for part in list:
+            if len(part) != len(set(part)):
+                return True
+
+    def getColumns(self):
+        columnList = []
+        for columnN in xrange(9): #looping column
+            column = []
+            for i in xrange(9): #looping row
+                column.append(self.board[i][columnN])
+            columnList.append(column)
+        return columnList
+
+    def getBox(self): #spent around 1 hour creating this -_-
+        boxlist = []
+        for Ysection in xrange(0,9,3): #[0,3,6]
+            for Xsection in xrange(0,9,3): #[0,3,6]
+                box = []
+                for rowN in xrange(3): #[0,1,2]
+                    box.extend(self.board[rowN + Ysection][Xsection:Xsection+3])
+                boxlist.append(box)
+        return boxlist
+
+    def removeZero(self,list):
+        removed = []
+        for part in list:
+            removed.append(filter(lambda n: n != 0, part))
+        return removed
 
 
 if __name__ == "__main__":
     g = Game()
     try:
         g.load()
+        dPrint("BOARD DATA:\n" + str(g.board) + "\n" + "-"*30)
+    except ValueError:
+        dPrint("[ ERROR ] Invalid character detected","error")
+        raise SystemExit
     except:
-        print "NO"
+        dPrint("[ ERROR ] Unknown error","error")
         raise SystemExit
-    if not g.verify():
-        print "NO"
+
+    verifyResult = g.verify()
+    if verifyResult[0] != "correct":
+        dPrint("[ " + verifyResult[0].upper() + " ] " + verifyResult[1].capitalize(), verifyResult[0])
         raise SystemExit
-    print "YES"
-    print g.board
+
+    #start solving it...
+    dPrint("[SUCCESS] The board follows sudoku rules","correct")
