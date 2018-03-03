617bd6315edd80ca838eb4fe47e25be2335d251f
Tony Tran <tonytrantrantony@gmail.com>
2018-03-03 23:02:29 +0100

    sudoku/sudoku.py: Added function 'possibilities'
    Function possibilities requires no argument and returns a 3D list
    where the first layer is the board, the second layer are the rows
    and the third layer are the tiles. the tiles can be None for tiles
    that has already a number, and for tiles with zero aka tiles that
    are blank/empty, there would be a list containing possible numbers
    filled in that particular tile.
    
    Also added function 'calculatePossible', which is essential for
    function 'possibilities'. This argument is the particular tile
    'row number' and 'column number'. With the argument, the function
    will get the particular row, column and small box, calculate what
    number are possible to fill and return the possible numbers in the
    particular empty tile (list format).
    
    Finally, also added function 'boxNumber' which converts the 'row
    number' and 'column number' into 'small box number'. The function
    will need to be rewrited due to the fact that it uses an 'ugly'
    way to calculate the 'small box number'.

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -94,6 +94,39 @@ class Game:
             removed.append(filter(lambda n: n != 0, part))
         return removed
 
+    def boxNumber(self,rowN,columnN): #UGLY SOLUTION! need to be rewrited!
+        data = [[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8]]
+        boxN = data[rowN][columnN]
+        return boxN
+
+    def possibilities(self):
+        list = []
+        rowN = 0
+        for row in self.board:
+            newRow = []
+            columnN = 0
+            for tile in row:
+                if tile == 0:
+                    possibleNum = self.calculatePossible(rowN,columnN)
+                else:
+                    possibleNum = None
+                newRow.append(possibleNum)
+                columnN = columnN + 1
+            list.append(newRow)
+            rowN = rowN + 1
+        return list
+
+    def calculatePossible(self,rowN,columnN):
+        possibleNum = [1,2,3,4,5,6,7,8,9]
+        row = self.board[rowN]
+        column = self.getColumns()[columnN] #can be more efficient
+        box = self.getBox()[self.boxNumber(rowN,columnN)]
+
+        for list in row, column, box:
+            for number in list:
+                if number in possibleNum:
+                    possibleNum.remove(number)
+        return possibleNum
 
 if __name__ == "__main__":
     g = Game()
@@ -114,3 +147,4 @@ if __name__ == "__main__":
 
     #start solving it...
     dPrint("[SUCCESS] The board follows sudoku rules","correct")
+    #print g.possibilities()
