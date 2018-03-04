35e4cb8448d1b06be2411b278433a247cdbbe249
Saruman <saruman@libertymail.net>
2018-03-05 00:04:12 +0100

    sudoku/test_solving.sh: fixed bug introduced in previous commit
    commit bd87a083c61195b335adb4436bf8a18b86b2d91d
    Author: Saruman <saruman@libertymail.net>
    Date:   Mon Mar 5 00:00:29 2018 +0100
    
    The above commit has introduced a bug into the test_solving.sh script.
    While `ls somedir` shows only the files in the dir, `ls somedir/*.dat`
    shows the files with the paths. Due to that, the 'solving_tests/' dir
    name has to be removed.

--- sudoku/test_solving.sh
+++ sudoku/test_solving.sh
@@ -10,7 +10,7 @@ SOLVING_OPTION=--solve
 FAILED=0
 
 for testcase in `ls solving_tests/*.dat`; do
-	RESULT=`$TOOL $SOLVING_OPTION < solving_tests/$testcase | tail -n 1`
+	RESULT=`$TOOL $SOLVING_OPTION < $testcase | tail -n 1`
 	if [ "$RESULT" = "solved" ]; then
 		echo "solving test :: `basename $testcase .dat` :: PASS"
 	else
