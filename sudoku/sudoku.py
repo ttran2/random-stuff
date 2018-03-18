51a4d15ef0ccfec7c4c694c32fe5a3b1540d7bee
Tony Tran <tonytrantrantony@gmail.com>
2018-03-18 01:47:45 +0100

    sudoku/sudoku.py: Upgrade it to be able to solve 'easy' sudoku
    --- Significant changes:
    
    Added function 'solvingLoop' (line 154), does not require any
    argument, return True or False depending whether it solved the
    puzzle or not. As the name suggests, it is a loop that solves
    the puzzle by loopubg function 'onePossible' and 'genPossibilities'
    
    Added function 'onePossible' (line 166), does not require any
    argument, return a number of tiles that had only one possible
    number. It checks for tiles that has only one possible number
    and 'insert' the respective number into the tile.
    
    Added function 'insert' (line 181), require 3 argments which are
    a value, row number and column number, returns nothing.
    The function simply insert a specific value (number) into a specific
    tile which can be located using the row number and column number.
    
    Added function 'boardPrint' (line 184), does not require any
    argument, returns nothing. Simply print out the default board
    in a user-friendly way (meaning the printed board is splitted into
    9 smaller sudoku squares).
    
    --- Smaller changes:
    
    Changed function 'possibilities' name to 'genPossibilities' which
    stands for 'generate possibilities'.
    
    Added function 'checkSolved' (line 81), does not require any
    argument, return True or False depending on whether the puzzle is
    solved (no empty tiles) or not.
    
    Function 'genPossibilities' creates a class variable 'possibilities'
    (self.possibilities).
    
    Modified the last part of 'verify' function, where now it will
    use the 'checkSolved' function.
    
    Added function 'color' (line 25), its commented out, does not require
    any argument, returns nothing, creates global variable 'cDefault'
    and 'cColor'. The function is initiated at the start of the program.
    It checks for user puting arguments: '-c', '--color',
    '--colors', 'color', or 'colors'.
    
    Modified 'getBox' function with better 'smarter' solution.
    
    In the possibilities board, tiles that are already filled in are
    not labeled as None anymore, instead they are empty tiles [].

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -21,6 +21,22 @@ def dPrint(string,alternative=None):
     if alternative:
         print alternative
 
+"""
+def color():
+    try:
+        global cDefault, cColor
+        if sys.argv[1] in ["-c","--color","--colors","color","colors"]:
+            from colorama import init, Fore
+            init()
+            cDefault = Fore.WHITE
+            cColor = Fore.RED
+        else:
+            print "NO COLOR"
+            cDefault = cColor = ""
+    except IndexError:
+        pass
+"""
+
 class Game:
     def __init__(self):
         self.board = []
@@ -57,11 +73,17 @@ class Game:
         if self.checkUnique(self.getBox()): #check in box
             return "incorrect","repeated number in a small box"
 
+        if self.checkSolved():
+            return "solved","the game is already solved"
+        else:
+            return "correct",""
+
+    def checkSolved(self):
         #check if any 'empty spots' (zeros)
         for i in xrange(9):
             if len(self.removeZero(self.board)[i]) != len(self.board[i]):
-                return "correct","" #there is zero therefore its not solved
-        return "solved","the game is already solved"
+                return False #there is zero therefore its not solved
+        return True #board is solved
 
     def checkUnique(self,list): #True = not unique, none = unique
         list = self.removeZero(list)
@@ -95,11 +117,12 @@ class Game:
         return removed
 
     def boxNumber(self,rowN,columnN): #UGLY SOLUTION! need to be rewrited!
-        data = [[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8]]
-        boxN = data[rowN][columnN]
-        return boxN
+        #data = [[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[0,0,0,1,1,1,2,2,2],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[3,3,3,4,4,4,5,5,5],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8],[6,6,6,7,7,7,8,8,8]]
+        #boxN = data[rowN][columnN]
+        #return boxN
+        return (columnN/3 + rowN - (rowN % 3))
 
-    def possibilities(self):
+    def genPossibilities(self):
         list = []
         rowN = 0
         for row in self.board:
@@ -109,12 +132,12 @@ class Game:
                 if tile == 0:
                     possibleNum = self.calculatePossible(rowN,columnN)
                 else:
-                    possibleNum = None
+                    possibleNum = []
                 newRow.append(possibleNum)
                 columnN = columnN + 1
             list.append(newRow)
             rowN = rowN + 1
-        return list
+        self.possibilities = list
 
     def calculatePossible(self,rowN,columnN):
         possibleNum = [1,2,3,4,5,6,7,8,9]
@@ -128,7 +151,54 @@ class Game:
                     possibleNum.remove(number)
         return possibleNum
 
+    def solvingLoop(self):
+        while True:
+            self.genPossibilities()
+            tilesFilled = self.onePossible() #number of tiles that had one possible number
+            if tilesFilled == 0:
+                if not self.checkSolved():
+                    dPrint("Sorry, this puzzle is out of my ability :-(")
+                    return False
+                else:
+                    dPrint("Mission accomplished! :-D")
+                    return True
+
+    def onePossible(self):
+        possBoard = self.possibilities
+        successNumber = 0
+        rowN = 0
+        for row in possBoard:
+            columnN = 0
+            for tile in row:
+                if len(tile) == 1:
+                    self.insert(tile[0],rowN,columnN) #tile number, row number, column number
+                    successNumber = successNumber + 1
+                columnN = columnN + 1
+            rowN = rowN + 1
+        dPrint("I found " + str(successNumber) + " tile(s) that have one possible number.")
+        return successNumber
+
+    def insert(self,value,rowN,columnN):
+        self.board[rowN][columnN] = value
+
+    def boardPrint(self):
+        rowN = 0
+        for row in self.board:
+            if rowN == 3:
+                print ""
+                rowN = 0
+            columnN = 0
+            for tile in row:
+                if columnN == 3:
+                    print "",
+                    columnN = 0
+                print str(tile),
+                columnN = columnN + 1
+            print ""
+            rowN = rowN + 1
+
 if __name__ == "__main__":
+    #color()
     g = Game()
     try:
         g.load()
@@ -147,4 +217,7 @@ if __name__ == "__main__":
 
     #start solving it...
     dPrint("[SUCCESS] The board follows sudoku rules","correct")
-    #print g.possibilities()
+    if g.solvingLoop():
+        g.boardPrint()
+    else:
+        dPrint("\n" + "-"*30 + "BOARD DATA:\n" + str(g.board) + "\n" + "-"*30)
