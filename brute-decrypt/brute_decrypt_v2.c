585fd904c79f746b510552ef0a3d671f0f3ad21b
Tony Tran <tonytrantrantony@gmail.com>
2018-12-13 20:36:06 +0100

    brute-decrypt/brute_decrypt_v2.c: Initial commit (upgrading to v2)
    Upgraded the original brute_decrypt.c from just using a predefined
    THRESHOLD, into a script that checks all possible keys (1-255) and
    determine the 'correct' key as the key with the most 'valid'
    characters (which are letters from a-z and a space).
    
    Added a STRUCTURE called 'best_t' which basically holds the best
    key and number of character.
    
    Instead of 'readability()' function, there is 'validChar()'
    function which returns the number of 'valid' characers which
    are letters from 'a' to 'z' and space.
    
    Script works fine on most of the test sample however on the test
    file crypto127 it truncated it.

---
+++ brute-decrypt/brute_decrypt_v2.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> // for tolower() and toupper()
+
+/* constants */
+#define MAX_SIZE 65536
+
+
+/* declare a structure (basically downgraded object) */
+typedef struct { // 'struct' is the KEYWORD (not custom)
+	int key;
+	int validChar;
+} best_t;
+
+/* Returns number of 'valid' characters (a-z and space) */
+int validChar(char *buf, int size)
+{
+	/* declare stuff */
+	int i, counter;
+
+	/* count integer */
+	counter = 0;
+	for (i = 0; i < size; i++)
+	{
+		if ( tolower(buf[i]) >= 'a' &&  tolower(buf[i]) <= 'z')
+		{
+			counter++;
+		}
+		if ( buf[i] == ' ')
+		{
+			counter++;
+		}
+	}
+
+	#ifdef DEBUG
+	fprintf(stderr, "The counter: %i\n", counter);
+	#endif
+
+	return counter;
+}
+
+/* Main function */
+int main(void)
+{
+	/* declare stuff */
+	int i, key, size;
+
+	/* allocate memory for a buffer 'buf' */
+	char *buf = malloc((MAX_SIZE + 1) * sizeof(char)); // buffer
+
+	/* check if buffer was allocated successfully */
+	if (buf == NULL)
+	{
+		fprintf(stderr, "FATAL: Failed to allocate memory for 'encrMsg' :(\n");
+		return 1;
+	}
+
+	/* allocate memory for 'encrMsg' */
+	char *encrMsg = malloc((MAX_SIZE + 1) * sizeof(char));
+
+	/* check encrMsg allocation */
+	if (encrMsg == NULL)
+	{
+		fprintf(stderr, "FATAL: Failed to allocate memory for 'encrMsg' :(\n");
+		free(buf);
+		return 1;
+	}
+
+	/* store STDIN into "encMsg" through a buffer */
+	fgets(encrMsg, MAX_SIZE, stdin);
+
+	/* get size of the STDIN */
+	size = strlen(encrMsg);
+
+	/* declare stuff for the brute force loop */
+	best_t best;
+	int numOfChar;
+
+	/* nullify 'best' */
+	best.key = 0;
+	best.validChar = 0;
+
+	/*  BRUTE FORCE LOOP */
+	for (key = 1; key < 256; key++)
+	{
+		#ifdef DEBUG
+		fprintf(stderr, "Checking key: %d | ", key);
+		#endif
+
+		/* loop through the characters in the encrypted msg */
+		for (i = 0; i < size; i++)
+		{
+			buf[i] = encrMsg[i]^key;
+		}
+
+		/* get the number of 'valid' characters */
+		numOfChar = validChar(buf, size);
+
+		if (best.validChar < numOfChar)
+		{
+			best.validChar = numOfChar;
+			best.key = key;
+		}
+	}
+
+	/* print out the results */
+	fprintf(stderr, "Best key: %d (%d 'valid' characters)\n", best.key, best.validChar);
+
+	/* print out decrypted msg*/
+	for (i = 0; i < size; i++)
+	{
+		putchar(encrMsg[i]^best.key);
+	}
+
+	/* clean after myself */
+	free(buf);
+	free(encrMsg);
+
+	return 0;
+}
